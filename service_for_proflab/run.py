2a618178c9b9d7fbbadb3b5c9c01e4cf029ace1f
Tigran Davtyan <tigran.davtyand@gmail.com>
2023-08-24 09:42:45 +0400

    Add cron job at 20:00 GMT 0

--- service_for_proflab/run.py
+++ service_for_proflab/run.py
@@ -6,6 +6,6 @@ from main.service import app, scheduler, job
 from config.config import Config
 
 if __name__ == "__main__":
-    scheduler.add_job(id ='job',func=job, trigger='cron', hour=18, minute=33)
+    scheduler.add_job(id ='job',func=job, trigger='cron', hour=20, minute=0)
     scheduler.start()
     serve(app, port=Config.APP_PORT)
